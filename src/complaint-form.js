01c251290ecc4561580ec2405802b7b6af4de616
Cameron Hatch <cameronlavigne92@gmail.com>
2018-10-10 12:58:15 -0500

    added input components

--- src/complaint-form.js
+++ src/complaint-form.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import {Field, reduxForm} from 'redux-form';
 import "./complaint-form.css";
+import {required, nonEmpty, fiveChar, charNumber} from './validators';
+import Input from './input';
 
 class ComplaintForm extends React.Component {
   render() {
@@ -9,18 +11,19 @@ class ComplaintForm extends React.Component {
       <form onSubmit={this.props.handleSubmit(values =>
         console.log(values)
         )}>
-        <label htmlFor="tracking">Tracking number</label>
-        <Field component="input" type="text" name="tracking" id="tracking" />
-        <label htmlFor="issue">What is your issue?</label>
-        <Field component="select" name="issue" id="issue">
+        <Field component={Input} element="input" 
+          type="text" name="tracking" id="tracking" label="Tracking Number"
+          validate={[required, nonEmpty, fiveChar, charNumber]} />
+        <Field component={Input} element="select" name="issue" id="issue"
+          label="What is your issue?">
           <option value='not-delivered'>My delivery hasn't arrived</option>
           <option value='wrong-item'>The wrong item was delivered</option>
           <option value='missing-part'>Part of my order was missing</option>
           <option value='damaged'>Some of my order arrived damaged</option>
           <option value='other'>Other (give details below)</option>
         </Field>
-        <label htmlFor="details">Give more details (optional)</label>
-        <Field component="textarea" name="details" id="details" />
+        <Field component={Input} element="textarea" name="details" id="details"
+          label="Give more details (optional)" />
         <button type="submit">Submit</button>
       </form>
     );
